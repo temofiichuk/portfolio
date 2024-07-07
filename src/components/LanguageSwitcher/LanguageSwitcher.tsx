d265e9e7b7a09ea3777c6c5d470ad5e31da72365
mihail <mihail.temofiichuk@gmail.com>
2024-07-07 11:13:08 +0200

    auto position of language switcher popup

--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -3,11 +3,13 @@ import styles from "./LanguageSwitcher.module.scss";
 import { usePathname, useRouter } from "next/navigation";
 import { useTranslation } from "react-i18next";
 import { i18n as i18nConfig } from "../../../i18n.config";
-import React, { HTMLAttributes, MouseEvent, useState } from "react";
+import React, { HTMLAttributes, MouseEvent, useEffect, useRef, useState } from "react";
 import { RiTranslate2 } from "@remixicon/react";
 import useOutsideEvent from "@/hooks/useOutsideEvent";
 
-interface ILanguageSwitcher extends HTMLAttributes<HTMLDivElement> {}
+interface ILanguageSwitcher extends HTMLAttributes<HTMLDivElement> {
+	popUpClassName?: string;
+}
 
 const languages = [
 	{ key: "en", full_name: "English" },
@@ -15,8 +17,9 @@ const languages = [
 	{ key: "ua", full_name: "Ukrainian" },
 ];
 
-const LanguageSwitcher = (props: ILanguageSwitcher) => {
+const LanguageSwitcher = ({ popUpClassName, ...props }: ILanguageSwitcher) => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [position, setPosition] = useState<{ top: number; left: number }>({ top: 0, left: 0 });
 
 	const router = useRouter();
 	const currentPathname = usePathname();
@@ -25,6 +28,8 @@ const LanguageSwitcher = (props: ILanguageSwitcher) => {
 	} = useTranslation();
 
 	const ref = useOutsideEvent<HTMLDivElement>("click", () => setIsOpen(false));
+	const refButton = useRef<HTMLButtonElement>(null);
+	const refSwitcher = useRef<HTMLUListElement>(null);
 
 	const handleChange = (e: MouseEvent) => {
 		const target = e.target as HTMLButtonElement;
@@ -47,14 +52,39 @@ const LanguageSwitcher = (props: ILanguageSwitcher) => {
 		router.refresh();
 	};
 
+	useEffect(() => {
+		if (!refSwitcher.current || !refButton.current) return;
+		const button = refButton.current;
+		const buttonRect = button.getBoundingClientRect();
+		const popup = refSwitcher.current;
+
+		// Default position: below the parent element
+		let { top, left } = buttonRect;
+
+		// // Adjust position if popup is out of viewport
+		if (left + popup.offsetWidth + button.offsetWidth >= window.innerWidth) {
+			left = (popup.offsetWidth - button.offsetWidth) * -1;
+		} else {
+			left = 0;
+		}
+
+		if (top + popup.offsetHeight + button.offsetHeight >= window.innerHeight) {
+			top = popup.offsetHeight * -1;
+		} else {
+			top = button.offsetHeight;
+		}
+
+		setPosition({ left, top });
+	}, [isOpen]);
+
 	return (
-		<div ref={ref} {...props} className={`${props.className} ${styles.languageSwitcher}`}>
-			<button className={styles.button} onClick={() => setIsOpen((prev) => !prev)}>
+		<div ref={ref} {...props} className={`${props.className ?? ""} ${styles.languageSwitcher}`}>
+			<button ref={refButton} className={styles.button} onClick={() => setIsOpen((prev) => !prev)}>
 				<RiTranslate2 widths={20} />
 			</button>
-			<div className={styles.wrapper} data-active={isOpen}>
+			<div style={{ ...position }} className={styles.wrapper} data-active={isOpen}>
 				<div>
-					<ul>
+					<ul ref={refSwitcher}>
 						{languages.map(({ key, full_name }) => (
 							<li key={key} data-checked={key === currentLocale}>
 								<button onClick={handleChange} value={key}>
